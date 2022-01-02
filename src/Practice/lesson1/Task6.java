fb10524843e92bb1810c61468648847c1dc8f856
e-nikoly <e-nikoly@bk.ru>
2022-01-02 23:13:48 +0700

    add

---
+++ src/Practice/lesson1/Task6.java
@@ -0,0 +1,11 @@
+package Practice.lesson1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int k=5;
+        for (int i=1; i<=9; i++){
+            System.out.println(i + " x " + k + " = " + i*k);
+            System.out.println();
+        }
+    }
+}
