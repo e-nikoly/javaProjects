d7f784dc85895751536a906edec9726d16cc2d2f
e-nikoly <e-nikoly@bk.ru>
2022-01-03 12:15:36 +0700

    add

--- src/Practice/lesson1/Task1.java
+++ src/Practice/lesson1/Task1.java
@@ -1,3 +1,11 @@
+/**
+ * 1. Вывести на экран слово “JAVA”, в строку, чтобы оно повторилось 10 раз, используя
+ * цикл while.
+ * Вывод в консоль должен быть таким:
+ * JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA
+ */
+
+
 package Practice.lesson1;
 
 public class Task1 {
